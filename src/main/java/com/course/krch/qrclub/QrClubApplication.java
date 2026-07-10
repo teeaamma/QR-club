69e1f3da808bb719a35d686380f66809b40e5b04
Timofey Chervov <chervov.timofey@yandex.ru>
2026-07-10 13:52:24 +0700

    Initial commit

---
+++ src/main/java/com/course/krch/qrclub/QrClubApplication.java
@@ -0,0 +1,13 @@
+package com.course.krch.qrclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrClubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/course/krch/qrclub/QrClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.krch.qrclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
